37b70084895f20379ca3b852f78c4a97c1df7bc5
artem <ivashenco45@gmail.com>
2020-09-14 02:22:38 +0300

    [feature] create 2 view post_detail and post_list

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    pass
+    posts = Post.objects.all()
+    return render(request, 'blog/base.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
