fa51a64db36504263b980147a202f099acf8241a
artem <ivashenco45@gmail.com>
2020-09-14 02:20:53 +0300

    [feature] create Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from django.urls import reverse
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    photo_title = models.ImageField(upload_to='title/', verbose_name='Титульное фото', blank=True)
+    title = models.CharField(max_length=200, verbose_name='Заглавие')
+    slug = models.SlugField(max_length=200)
+    body = models.TextField(verbose_name='Текст')
+    publish = models.DateTimeField(default=now, verbose_name='Дата публикации')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
+
+    class Meta:
+        ordering = ['-publish']
