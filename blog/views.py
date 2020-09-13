54adf8570603b4c716c916990264169b1d311513
artem <ivashenco45@gmail.com>
2020-09-14 02:25:39 +0300

    [feature] correct url to base template in render func

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/base.html', {'posts': posts})
+    return render(request, 'base.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/detail.html', {'post': post})
+    return render(request, 'base.html', {'post': post})
