c49cc76155037ecc15d314dccca872a722a888bf
artem <ivashenco45@gmail.com>
2020-09-14 16:43:01 +0300

    [fix] fix uncorrect urls

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('sign_up/', views.register, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('sign_up/', views.register, name='register'),
+    path('logout/', views.log_out, name='logout'),
     path('sign_in/', views.log_in, name='login')
 ]
