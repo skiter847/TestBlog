d3ee8e031c7e8aa2f222cdc77c77395462c5f33f
artem <ivashenco45@gmail.com>
2020-09-14 16:43:49 +0300

    [feature] add log_in view

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .forms import RegistrationUserForm
+from django.shortcuts import render, redirect, reverse
+from .forms import RegistrationUserForm, LoginUserForm
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, login, authenticate
 
@@ -15,11 +15,28 @@ def register(request):
             new_user.set_password(cd['password'])
             new_user.save()
 
-            return redirect("blog:post_list")
+            return redirect("account:login")
 
-    return render(request, 'account/login.html', {'form': form})
+    return render(request, 'account/register.html', {'form': form})
 
 
 def log_out(request):
     logout(request)
     return redirect("account:login")
+
+
+def log_in(request):
+    form = LoginUserForm(request)
+
+    if request.method == 'POST':
+        form = LoginUserForm(request, request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['user']:
+                login(request, cd['user'])
+
+            return redirect("blog:post_list")
+
+    return render(request, 'account/login.html', {
+        'form': form
+    })
