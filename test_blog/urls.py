e5d49af547c24a076dba27c0dfb2ca256bce4d32
artem <ivashenco45@gmail.com>
2020-09-14 13:09:01 +0300

    [feature] add url to account app

--- test_blog/urls.py
+++ test_blog/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    path('account/', include('account.urls', namespace='account'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
